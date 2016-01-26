9f436b228f3f713a83e77157debc37507e41e469
Timon <mail@weeaboo.nl>
2016-01-26 20:34:20 +0100

    Unit test for LuaScriptUtil

--- core/src/main/java/nl/weeaboo/vn/script/lua/LuaScriptUtil.java
+++ core/src/main/java/nl/weeaboo/vn/script/lua/LuaScriptUtil.java
@@ -1,13 +1,13 @@
 package nl.weeaboo.vn.script.lua;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import org.luaj.vm2.LuaThread;
 import org.luaj.vm2.Varargs;
 import org.luaj.vm2.lib.DebugLib;
+
+import com.google.common.collect.ImmutableList;
 
 import nl.weeaboo.lua2.LuaException;
 import nl.weeaboo.lua2.LuaRunState;
@@ -16,6 +16,7 @@ import nl.weeaboo.vn.core.IContext;
 import nl.weeaboo.vn.core.ResourceLoadInfo;
 import nl.weeaboo.vn.core.impl.ContextUtil;
 import nl.weeaboo.vn.script.IScriptLoader;
+import nl.weeaboo.vn.script.IScriptThread;
 import nl.weeaboo.vn.script.ScriptException;
 
 public final class LuaScriptUtil {
@@ -30,10 +31,15 @@ public final class LuaScriptUtil {
         return filename.endsWith(".lvn");
     }
 
+    /**
+     * Creates a {@link LuaScriptFunction} from the closure stored in the given vararg in the given position.
+     * All remaining arguments in the vararg are passed as parameters to the returned function.
+     */
     public static LuaScriptFunction toScriptFunction(Varargs args, int offset) {
         return new LuaScriptFunction(args.checkclosure(offset), args.subargs(offset+1));
     }
 
+    /** Converts a {@link LuaException} to a {@link ScriptException} */
     public static ScriptException toScriptException(String message, LuaException e) {
         ScriptException se = new ScriptException(message + ": " + e.getMessage());
         se.setStackTrace(e.getStackTrace());
@@ -41,22 +47,24 @@ public final class LuaScriptUtil {
         return se;
     }
 
+    /**
+     * Creates a persistent script thread.
+     *
+     * @see LuaLink#setPersistent(boolean)
+     */
     public static LuaScriptThread createPersistentThread(LuaRunState runState) {
         LuaLink luaLink = new LuaLink(runState);
         luaLink.setPersistent(true);
         return new LuaScriptThread(luaLink);
     }
 
+    /** @return The current call stack of the active Lua thread, or an empty list if no thread is active. */
     public static List<String> getLuaStack() {
         return getLuaStack(LuaThread.getRunning());
     }
 
-    public static List<String> getLuaStack(LuaThread thread) {
-        if (thread == null) {
-            return Collections.emptyList();
-        }
-
-        List<String> result = new ArrayList<String>();
+    static List<String> getLuaStack(LuaThread thread) {
+        ImmutableList.Builder<String> result = ImmutableList.builder();
         for (int level = 0; level < DEFAULT_STACK_LIMIT; level++) {
             String line = DebugLib.fileline(thread, level);
             if (line == null) {
@@ -64,28 +72,7 @@ public final class LuaScriptUtil {
             }
             result.add(line);
         }
-        return result;
-    }
-
-    public static String getNearestLvnSrcloc(LuaScriptThread thread) {
-        return getNearestLvnSrcloc(thread.luaLink.getThread());
-    }
-
-    private static String getNearestLvnSrcloc(LuaThread thread) {
-        if (thread == null) {
-            return null;
-        }
-        for (int level = 0; level < DEFAULT_STACK_LIMIT; level++) {
-            String line = DebugLib.fileline(thread, level);
-            if (line == null) {
-                break;
-            }
-
-            if (line.contains(LVN_PATTERN)) {
-                return line;
-            }
-        }
-        return null;
+        return result.build();
     }
 
     public static String getNearestLvnSrcloc(List<String> stack) {
@@ -101,6 +88,15 @@ public final class LuaScriptUtil {
         return new ResourceLoadInfo(filename, getLuaStack());
     }
 
+    private static LuaScriptContext getScriptContext(IContext context) {
+        return (LuaScriptContext)context.getScriptContext();
+    }
+
+    /**
+     * Loads a script in the main thread of the given context.
+     *
+     * @see IScriptLoader#loadScript(IScriptThread, String)
+     */
     public static void loadScript(IContext mainContext, IScriptLoader scriptLoader, String scriptFilename)
             throws IOException, ScriptException {
 
@@ -115,10 +111,11 @@ public final class LuaScriptUtil {
         }
     }
 
-    private static LuaScriptContext getScriptContext(IContext context) {
-        return (LuaScriptContext)context.getScriptContext();
-    }
-
+    /**
+     * Calls a function in the main thread of the given context.
+     *
+     * @see IScriptLoader#loadScript(IScriptThread, String)
+     */
     public static void callFunction(IContext mainContext, String functionName, Object... args)
             throws ScriptException {
 
@@ -133,6 +130,11 @@ public final class LuaScriptUtil {
         }
     }
 
+    /**
+     * Runs arbitrary Lua code in the main thread of the given context.
+     *
+     * @see IScriptLoader#loadScript(IScriptThread, String)
+     */
     public static String eval(IContext mainContext, String luaCode) throws ScriptException {
         LuaScriptContext scriptContext = getScriptContext(mainContext);
         LuaScriptThread mainThread = scriptContext.getMainThread();
