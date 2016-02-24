0d246205f06b9214ff5891834190a9c8f6edb5b3
Timon <mail@weeaboo.nl>
2016-02-24 18:23:04 +0100

    Get rid of second RenderUtil class

--- core/src/main/java/nl/weeaboo/vn/core/impl/RenderEnv.java
+++ core/src/main/java/nl/weeaboo/vn/core/impl/RenderEnv.java
@@ -32,9 +32,26 @@ public final class RenderEnv implements IRenderEnv {
 
 		this.scale = Math.min(rclip.w / (double)vsize.w, rclip.h / (double)vsize.h);
 		this.glClip = Rect.of(rclip.x, rscreen.h - rclip.y - rclip.h, rclip.w, rclip.h);
-		this.glScreenVirtualBounds = RenderUtil.calculateGLScreenVirtualBounds(rclip.x, rclip.y,
+        this.glScreenVirtualBounds = calculateGLScreenVirtualBounds(rclip.x, rclip.y,
 				rscreen.w, rscreen.h, scale);
 	}
+
+    /**
+     * @param scale The scale factor from virtual coordinates to real coordinates.
+     */
+    private static Rect2D calculateGLScreenVirtualBounds(int clipX, int clipY, int screenWidth,
+            int screenHeight, double scale) {
+        double s = 1.0 / scale;
+        double x = s * -clipX;
+        double y = s * -clipY;
+        double w = s * screenWidth;
+        double h = s * screenHeight;
+
+        w = Double.isNaN(w) ? 0 : Math.max(0, w);
+        h = Double.isNaN(h) ? 0 : Math.max(0, h);
+
+        return Rect2D.of(x, y, w, h);
+    }
 
     public static RenderEnv newDefaultInstance(Dim vsize, boolean isTouchScreen) {
         Rect rclip = Rect.of(0, 0, vsize.w, vsize.h);
