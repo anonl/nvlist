3eed22cc9576c971223fb34693abd3ce537dbb1c
Timon <mail@weeaboo.nl>
2016-03-05 20:26:51 +0100

    Move LuaJPP2 to its own repository

--- LuaJPP2/src/main/java/nl/weeaboo/lua2/io/DelayedReader.java
+++
@@ -1,7 +0,0 @@
-package nl.weeaboo.lua2.io;
-
-public interface DelayedReader {
-
-	public void onRead(Object obj);
-	
-}
--- LuaJPP2/src/main/java/nl/weeaboo/lua2/io/LuaSerializable.java
+++
@@ -1,9 +0,0 @@
-package nl.weeaboo.lua2.io;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface LuaSerializable {
-	
-}
