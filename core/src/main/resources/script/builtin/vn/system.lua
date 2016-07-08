a4ef4392410828fafa6e391e8f480eb0dd4a2379
Timon <mail@weeaboo.nl>
2016-07-08 21:43:08 +0200

    Implement system.lua setExitFunction()

--- core/src/main/resources/script/builtin/vn/system.lua
+++ core/src/main/resources/script/builtin/vn/system.lua
@@ -6,13 +6,26 @@ module("vn.system", package.seeall)
 --  Variables
 -- ----------------------------------------------------------------------------
 
+local exitFunction = nil
+
 -- ----------------------------------------------------------------------------
 --  Callbacks 
 -- ----------------------------------------------------------------------------
 
 -- This function is called when the user presses the window close button or exit option from the window menu.
 function onExit()
-    System.exit(true)
+    if exitFunction == nil then
+        return System.exit(true)
+    end
+
+    local ctxt = createContext(function()
+        if exitFunction() then
+            System.exit(true)
+            return
+        end
+        getCurrentContext():destroy()
+    end)
+    setContextActive(ctxt, true)
 end
 
 -- ----------------------------------------------------------------------------
@@ -36,6 +49,14 @@ end
 --      user will be presented with a confirmation popup.
 function exit(force)
     return System.exit(force)
+end
+
+---Sets a custom function to be called when the user tries to exit the program or when the <code>exit</code>
+-- function is called. This starts a new mode, &quot;exit&quot; in which the supplied exit function is called.
+-- @func func The function to call when the &quot;exit&quot; submode is entered. If this function returns
+--       <code>false</code>, the exit process is cancelled.
+function setExitFunction(func)
+    exitFunction = func
 end
 
 ---Opens the website with the given <code>url</code> in an external web browser. 
