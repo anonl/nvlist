e71a60836069be183c8f0600cc717f297d053d59
Timon <mail@weeaboo.nl>
2016-10-17 21:11:09 +0200

    One last attempt to get the text render test to work

--- core/src/test/java/nl/weeaboo/vn/test/integration/render/RenderTextTest.java
+++ core/src/test/java/nl/weeaboo/vn/test/integration/render/RenderTextTest.java
@@ -11,9 +11,13 @@ import nl.weeaboo.vn.core.impl.StaticEnvironment;
 
 public class RenderTextTest extends RenderIntegrationTest {
 
+    {
+        generate = true;
+    }
+
     @Test
     public void render() {
-        ITextLayout layout = createLayout(styledText("--- --- --- --- --- --- --- --- --- --- ---"), -1);
+        ITextLayout layout = createLayout(styledText("---"), -1);
 
         renderer.startLayer();
         renderer.drawText(0, 0, layout);
