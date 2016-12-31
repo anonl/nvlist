d9f1cc1cab96b738e88c96633cb9230ac526d6a0
Timon <mail@weeaboo.nl>
2016-12-31 11:23:27 +0100

    Add VnId (unique identifier) for future use

---
+++ api/src/main/java/nl/weeaboo/vn/core/VnId.java
@@ -0,0 +1,24 @@
+package nl.weeaboo.vn.core;
+
+import nl.weeaboo.common.AbstractId;
+import nl.weeaboo.common.Checks;
+
+// TODO: Store in NovelPrefs
+public final class VnId extends AbstractId {
+
+    private static final long serialVersionUID = 1L;
+    private static final String REGEX = "[a-z][a-z0-9_]+";
+
+    public static final VnId UNKNOWN = new VnId("unknown");
+
+    public VnId(String id) {
+        super(id);
+
+        Checks.checkArgument(isValidId(id), "ID must be all lowercase alphnumeric (matching " + REGEX + ")");
+    }
+
+    public static boolean isValidId(String id) {
+        return id.matches(REGEX);
+    }
+
+}
