d8439a5ea7fc714bcfb6077d245d0ef456a8df87
Timon <mail@weeaboo.nl>
2017-01-25 20:47:24 +0100

    Use GL_NEAREST in render unit tests

--- core/src/test/java/nl/weeaboo/vn/test/integration/render/RenderIntegrationTest.java
+++ core/src/test/java/nl/weeaboo/vn/test/integration/render/RenderIntegrationTest.java
@@ -7,10 +7,12 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.PixmapIO;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
 
 import nl.weeaboo.common.Dim;
 import nl.weeaboo.common.Rect;
 import nl.weeaboo.filesystem.FilePath;
+import nl.weeaboo.gdx.graphics.GdxTextureUtil;
 import nl.weeaboo.gdx.test.pixmap.PixmapEquality;
 import nl.weeaboo.gdx.test.pixmap.ScreenshotHelper;
 import nl.weeaboo.vn.image.ITexture;
@@ -47,7 +49,10 @@ public abstract class RenderIntegrationTest extends IntegrationTest {
     }
 
     protected ITexture getTexture(String path) {
-        return env.getImageModule().getTexture(FilePath.of(path));
+        ITexture texture = env.getImageModule().getTexture(FilePath.of(path));
+        // Set filtering to nearest so we don't get trolled by slight interpolation differences on the build server
+        GdxTextureUtil.getTexture(texture).setFilter(TextureFilter.Nearest, TextureFilter.Nearest);
+        return texture;
     }
 
     public void checkRenderResult(String testName) {
