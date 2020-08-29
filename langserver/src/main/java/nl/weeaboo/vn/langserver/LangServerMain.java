461abf3c799d3a749c0a4038b2d5942b00616041
Timon <mail@weeaboo.nl>
2020-08-29 04:00:13 +0200

    Use sockets to connect langserver (stdin is unreliable when running inside Gradle)

--- langserver/src/main/java/nl/weeaboo/vn/langserver/LangServerMain.java
+++ langserver/src/main/java/nl/weeaboo/vn/langserver/LangServerMain.java
@@ -1,15 +1,42 @@
 package nl.weeaboo.vn.langserver;
 
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.io.Closer;
 
 import nl.weeaboo.vn.impl.InitConfig;
 
 final class LangServerMain {
 
+    private static final Logger LOG = LoggerFactory.getLogger(LangServerMain.class);
+
     public static void main(String[] args) throws IOException {
         InitConfig.init();
 
-        NvlistLangServer.start(System.in, System.out);
+        Closer closer = Closer.create();
+
+        ServerSocket serverSocket = new ServerSocket(12345);
+        closer.register(serverSocket);
+
+        Socket socket = serverSocket.accept();
+        closer.register(socket);
+
+        NvlistLangServer.start(socket.getInputStream(), socket.getOutputStream());
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                closer.close();
+            } catch (IOException e) {
+                LOG.warn("Error closing socket", e);
+            }
+        }));
+
+        // NvlistLangServer.start(System.in, System.out);
     }
 
 }
