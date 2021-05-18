e71d7b69981d89bb2bceb44f7131e5f38941b32f
Timon <mail@weeaboo.nl>
2021-05-18 20:26:03 +0200

    Revert API-incompatible change

--- api/src/main/java/nl/weeaboo/vn/script/IScriptLoader.java
+++ api/src/main/java/nl/weeaboo/vn/script/IScriptLoader.java
@@ -19,11 +19,24 @@ public interface IScriptLoader extends IResourceResolver {
      * @throws FileNotFoundException If the file could not be openend.
      * @throws IOException If an exception occurs while trying to open the script.
      */
-    public InputStream openScript(FilePath filename) throws FileNotFoundException, IOException;
+    InputStream openScript(FilePath filename) throws FileNotFoundException, IOException;
+
+    /**
+     * Executes a script file.
+     *
+     * @param thread The thread to execute the script on.
+     * @throws IOException If an exception occurs while trying to open the script.
+     * @throws ScriptException If an exception occurs while executing the script.
+     *
+     * @deprecated The behavior of this method depends on the currently running context, making it unsafe and
+     *             not very useful.
+     */
+    @Deprecated
+    void loadScript(IScriptThread thread, FilePath filename) throws IOException, ScriptException;
 
     /**
      * Returns the paths for all script files in the specified folder and its sub-folders.
      */
-    public Collection<FilePath> getScriptFiles(FilePath folder);
+    Collection<FilePath> getScriptFiles(FilePath folder);
 
 }
